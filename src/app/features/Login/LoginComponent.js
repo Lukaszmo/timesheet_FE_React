051d0c9504dfa44157c5e190327e1d336fa09ba3
Łukasz Molenda <lukasz.molenda@CCAEUROPE.LOCAL>
2020-03-20 16:20:28 +0100

    centered login container

--- src/app/features/Login/LoginComponent.js
+++ src/app/features/Login/LoginComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Form, Button, Segment } from 'semantic-ui-react';
+import { Form, Button, Header } from 'semantic-ui-react';
 import { Formik } from 'formik';
 import './LoginComponent.css';
 
@@ -14,55 +14,59 @@ class LoginComponent extends Component {
     render() {
 
         return (
+            <div className='login-container'>
+                <div className='login-header-container'>
+                    <Header as='h2' className='login-header'>Logowanie</Header>
+                </div>
+                <div className='inputs-login-container' >
+                    <Formik
+                        initialValues={{ login: '', password: '' }}
+                        //validacje
 
-            < Segment className='inputs-login-container' >
-                <Formik
-                    initialValues={{ login: '', password: '' }}
-                    //validacje
-
-                    onSubmit={(values, { setSubmitting }) => {
-                        this.onSubmit(values);
-                        setSubmitting(false);
-                    }}
-                >
-                    {({
-                        values,
-                        errors,
-                        touched,
-                        handleChange,
-                        handleBlur,
-                        handleSubmit,
-                        isSubmitting,
-                    }) => (
-                            <Form className='inputs-login-form' onSubmit={handleSubmit}>
-                                <Form.Field
-                                    name="login"
-                                    className='inputs-login-form-field'
-                                    placeholder="Login"
-                                    control='input'
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    value={values.login}
-                                />
-                                {errors.login && touched.login && errors.login}
-                                <Form.Field
-                                    type="password"
-                                    name="password"
-                                    className='inputs-login-form-field'
-                                    placeholder="Hasło"
-                                    control='input'
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    value={values.password}
-                                />
-                                {errors.password && touched.password && errors.password}
-                                <Button type="submit" color="teal" disabled={isSubmitting}>
-                                    ZALOGUJ SIĘ
+                        onSubmit={(values, { setSubmitting }) => {
+                            this.onSubmit(values);
+                            setSubmitting(false);
+                        }}
+                    >
+                        {({
+                            values,
+                            errors,
+                            touched,
+                            handleChange,
+                            handleBlur,
+                            handleSubmit,
+                            isSubmitting,
+                        }) => (
+                                <Form className='inputs-login-form' onSubmit={handleSubmit}>
+                                    <Form.Field
+                                        name="login"
+                                        className='inputs-login-form-field'
+                                        placeholder="Login"
+                                        control='input'
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.login}
+                                    />
+                                    {errors.login && touched.login && errors.login}
+                                    <Form.Field
+                                        type="password"
+                                        name="password"
+                                        className='inputs-login-form-field'
+                                        placeholder="Hasło"
+                                        control='input'
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.password}
+                                    />
+                                    {errors.password && touched.password && errors.password}
+                                    <Button type="submit" color="teal" disabled={isSubmitting}>
+                                        ZALOGUJ SIĘ
                                 </Button>
-                            </Form>
-                        )}
-                </Formik>
-            </Segment >
+                                </Form>
+                            )}
+                    </Formik>
+                </div>
+            </div >
 
         )
     }
