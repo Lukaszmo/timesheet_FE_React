5ae5663241c57945e0dac037227cb1174c38cbb2
Łukasz Molenda <lukasz.molenda@CCAEUROPE.LOCAL>
2020-06-30 19:41:36 +0200

    vacation request details

---
+++ src/app/features/VacationList/VacationRequestDetails.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+import { Formik } from 'formik';
+import { Modal, Button, Dropdown, Grid, Input, Form } from 'semantic-ui-react';
+import CustomLabel from '../../common/CustomLabel/CustomLabel';
+import './HoursComponent.css';
+
+
+class VacationRequestDetails extends Component {
+
+
+
+    render() {
+
+
+        return (
+
+            null
+
+        )
+    }
+}
+
+export default VacationRequestDetails;
