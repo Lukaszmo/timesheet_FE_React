28838f839f76b70da2aa9adb887a9b97a31e0f12
Łukasz Molenda <lukasz.molenda@CCAEUROPE.LOCAL>
2020-09-29 23:38:04 +0200

    added menu hamburger bar menu to allow show/hide menu in mobile devices

--- src/app/common/Main/MainComponent.js
+++ src/app/common/Main/MainComponent.js
@@ -1,9 +1,10 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 import MenuContainer from "../Menu/MenuContainer";
 import SubMenuContainer from "../Menu/SubMenuContainer";
 import './Main.css'
 import Header from "./Header";
+import Media from 'react-media';
 //import Footer from "./Footer";
 //import Content from "./Content/Content";
 
@@ -14,23 +15,38 @@ class MainComponent extends Component {
 
         const menuId = this.props.menuId;
 
-        let mainMenu = <MenuContainer menuId={this.props.menuId} open={this.props.open} />
         let subMenu = <SubMenuContainer data={this.props} submenuId={this.props.submenuId}></SubMenuContainer>
 
-        if ((menuId === 'HOME') || (this.props.open === false)) {
-
-            subMenu = null;
-        }
+        if (menuId === 'HOME') { subMenu = null; }
 
         return (
             <div className="main-container">
                 <Header menuId={menuId} open={this.props.open} setOpen={this.props.setOpen} />
-                {mainMenu}
 
+                <Media queries={{
+                    small: "(max-width: 599px)",                            //mobile
+                    medium: "(min-width: 600px) and (max-width: 1199px)",   //tablet
+                    large: "(min-width: 1200px)"                            //laptop
+                }}>
+                    {matches => (
+                        <Fragment>
+                            {(matches.large || matches.medium) &&
+                                <div>
+                                    <MenuContainer menuId={this.props.menuId} />
+                                    {subMenu}
+                                </div>}
+                            {(matches.small) && (this.props.open === true) &&
+                                //na urządzeniach mobilnych menu wyświetlamy tylko gdy użytkownik kliknął hamburgera
+                                <div>
+                                    <MenuContainer menuId={this.props.menuId} />
+                                    {subMenu}
+                                </div>}
+                        </Fragment>
+                    )}
+                </Media>
 
                 {/* <Content content={this.props.content} /> content przekazywany w App.js*/}
 
-                {subMenu}
                 {this.props.content}
 
                 {/*<Footer /> */}
--- src/app/common/Menu/MenuContainer.js
+++ src/app/common/Menu/MenuContainer.js
@@ -4,7 +4,6 @@ import MenuComponent from "./MenuComponent";
 import { setMenu } from "../Menu/Menu";
 import * as AuthService from '../../utils/AuthService';
 import { Redirect } from "react-router-dom";
-import Media from 'react-media';
 
 class MenuContainer extends Component {
 
@@ -16,29 +15,11 @@ class MenuContainer extends Component {
         }
 
         return (
+            <MenuComponent
+                menu={this.menu}
+                activeMenuItem={this.props.menuId}
+            />
 
-            <Media queries={{
-                small: "(max-width: 599px)",    //mobile
-                medium: "(min-width: 600px) and (max-width: 1199px)",   //tablet
-                large: "(min-width: 1200px)"    //laptop
-            }}>
-                {matches => (
-                    <Fragment>
-                        {(matches.large || matches.medium) &&
-                            <MenuComponent
-                                menu={this.menu}
-                                activeMenuItem={this.props.menuId}
-                            />}
-
-                        {(matches.small) && (this.props.open === true) &&
-                            //na urządzeniach mobilnych menu wyświetlamy tylko gdy użytkownik kliknął hamburgera
-                            <MenuComponent
-                                menu={this.menu}
-                                activeMenuItem={this.props.menuId}
-                            />}
-                    </Fragment>
-                )}
-            </Media>
         )
     }
 }
