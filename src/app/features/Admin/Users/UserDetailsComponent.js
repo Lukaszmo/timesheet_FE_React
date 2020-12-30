5472cb10ddc2b3b1082084c526e6fffd1697e4dc
Łukasz Molenda <lukasz.molenda@CCAEUROPE.LOCAL>
2020-12-31 00:04:40 +0100

    header name correction

--- src/app/features/Admin/Users/UserDetailsComponent.js
+++ src/app/features/Admin/Users/UserDetailsComponent.js
@@ -49,7 +49,7 @@ class UserDetailsComponent extends Component {
 
         return (
             <Segment color="teal" >
-                <Header size='medium'>Edycja uzytkownika</Header>
+                <Header size='medium'>Edycja użytkownika</Header>
                 <Divider></Divider>
 
                 <Formik
@@ -80,154 +80,154 @@ class UserDetailsComponent extends Component {
                         errors,
                         touched
                     }) => (
-                            <Form onSubmit={handleSubmit}>
+                        <Form onSubmit={handleSubmit}>
 
-                                <Grid columns={2} textAlign="right" verticalAlign="middle" >
-                                    <Grid.Row>
-                                        <Grid.Column width={2}>
-                                            Id użytkownika
+                            <Grid columns={2} textAlign="right" verticalAlign="middle" >
+                                <Grid.Row>
+                                    <Grid.Column width={2}>
+                                        Id użytkownika
                                         </Grid.Column >
-                                        <Grid.Column width={3}>
-                                            <p className='data-field'> {this.props.record.id} </p>
-                                        </Grid.Column>
-                                    </Grid.Row>
-                                    <Grid.Row>
-                                        <Grid.Column width={2}>
-                                            <p className='data-field-header'>Nazwa Użytkownika</p>
-                                        </Grid.Column >
-                                        <Grid.Column width={3}>
-                                            <Input
-                                                type="text"
-                                                name='username'
-                                                value={values.username}
-                                                onChange={(e, data) => this.handleChange(e, data, setFieldValue)} />
-                                            {errors.username && touched.username ? <div><CustomLabel text={errors.username}></CustomLabel></div> : null}
-                                        </Grid.Column>
-                                    </Grid.Row>
-                                    <Grid.Row>
-                                        <Grid.Column width={2}>
-                                            <p className='data-field-header'>Imię</p>
-                                        </Grid.Column >
-                                        <Grid.Column width={3}>
-                                            <Input
-                                                type="text"
-                                                name='firstname'
-                                                value={values.firstname}
-                                                onChange={(e, data) => this.handleChange(e, data, setFieldValue)} />
-                                            {errors.firstname && touched.firstname ? <div><CustomLabel text={errors.firstname}></CustomLabel></div> : null}
-                                        </Grid.Column>
-                                    </Grid.Row>
-                                    <Grid.Row>
-                                        <Grid.Column width={2}>
-                                            <p className='data-field-header'>Nazwisko</p>
-                                        </Grid.Column >
-                                        <Grid.Column width={3}>
-                                            <Input
-                                                type="text"
-                                                name='lastname'
-                                                value={values.lastname}
-                                                onChange={(e, data) => this.handleChange(e, data, setFieldValue)} />
-                                            {errors.lastname && touched.lastname ? <div><CustomLabel text={errors.lastname}></CustomLabel></div> : null}
-                                        </Grid.Column>
-                                    </Grid.Row>
-                                    <Grid.Row>
-                                        <Grid.Column width={2}>
-                                            <p className='data-field-header'>Numer ewidencyjny</p>
-                                        </Grid.Column >
-                                        <Grid.Column width={3}>
-                                            <Input
-                                                type="number"
-                                                name='regnum'
-                                                value={values.regnum}
-                                                onChange={(e, data) => this.handleChange(e, data, setFieldValue)}
-                                            />
-                                            {errors.regnum && touched.regnum ? <div><CustomLabel text={errors.regnum}></CustomLabel></div> : null}
-                                        </Grid.Column>
-                                    </Grid.Row>
-                                    <Grid.Row>
-                                        <Grid.Column width={2}>
-                                            <p className='data-field-header'>Email</p>
-                                        </Grid.Column >
-                                        <Grid.Column width={3}>
-                                            <Input
-                                                type="email"
-                                                name='email'
-                                                value={values.email}
-                                                onChange={(e, data) => this.handleChange(e, data, setFieldValue)} />
-                                            {errors.email && touched.email ? <div><CustomLabel text={errors.email}></CustomLabel></div> : null}
-                                        </Grid.Column>
-                                    </Grid.Row>
-                                    <Grid.Row>
-                                        <Grid.Column width={2}>
-                                            <p className='data-field-header'>Stanowisko</p>
-                                        </Grid.Column >
-                                        <Grid.Column width={3}>
-                                            <Input
-                                                type="text"
-                                                name='position'
-                                                value={values.position}
-                                                onChange={(e, data) => this.handleChange(e, data, setFieldValue)} />
-                                            {errors.position && touched.position ? <div><CustomLabel text={errors.position}></CustomLabel></div> : null}
-                                        </Grid.Column>
-                                    </Grid.Row>
-                                    <Grid.Row>
-                                        <Grid.Column width={2}>
-                                            <p className='data-field-header'>Przełożony</p>
-                                        </Grid.Column >
-                                        <Grid.Column width={3}>
-                                            <Dropdown fluid selection
-                                                name='manager'
-                                                className=''
-                                                placeholder="Wybierz.."
-                                                value={values.manager}
-                                                onChange={(e, data) => this.handleChange(e, data, setFieldValue)}
-                                                options={this.props.users} />
-                                            {errors.manager && touched.manager ? <div><CustomLabel text={errors.manager}></CustomLabel></div> : null}
-                                        </Grid.Column>
-                                    </Grid.Row>
-                                    <Grid.Row>
-                                        <Grid.Column width={2}>
-                                            <p className='data-field-header'>Rola użytkownika</p>
-                                        </Grid.Column >
-                                        <Grid.Column width={3}>
-                                            <Dropdown fluid selection
-                                                name='roles'
-                                                className=''
-                                                placeholder="Wybierz.."
-                                                value={values.roles}
-                                                onChange={(e, data) => this.handleChange(e, data, setFieldValue)}
-                                                options={this.props.roles} />
-                                            {errors.roles && touched.roles ? <div><CustomLabel text={errors.roles}></CustomLabel></div> : null}
-                                        </Grid.Column>
-                                    </Grid.Row>
-                                    <Grid.Row>
-                                        <Grid.Column width={2}>
-                                            <p className='data-field-header'>Aktywny</p>
-                                        </Grid.Column >
-                                        <Grid.Column width={3}>
-                                            <Checkbox
-                                                name='active'
-                                                className='checkbox-active'
-                                                checked={values.active}
-                                                onClick={(e, data) => this.handleChange(e, data, setFieldValue)}
-                                            />
-                                        </Grid.Column>
-                                    </Grid.Row>
-
-
+                                    <Grid.Column width={3}>
+                                        <p className='data-field'> {this.props.record.id} </p>
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column width={2}>
+                                        <p className='data-field-header'>Nazwa Użytkownika</p>
+                                    </Grid.Column >
+                                    <Grid.Column width={3}>
+                                        <Input
+                                            type="text"
+                                            name='username'
+                                            value={values.username}
+                                            onChange={(e, data) => this.handleChange(e, data, setFieldValue)} />
+                                        {errors.username && touched.username ? <div><CustomLabel text={errors.username}></CustomLabel></div> : null}
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column width={2}>
+                                        <p className='data-field-header'>Imię</p>
+                                    </Grid.Column >
+                                    <Grid.Column width={3}>
+                                        <Input
+                                            type="text"
+                                            name='firstname'
+                                            value={values.firstname}
+                                            onChange={(e, data) => this.handleChange(e, data, setFieldValue)} />
+                                        {errors.firstname && touched.firstname ? <div><CustomLabel text={errors.firstname}></CustomLabel></div> : null}
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column width={2}>
+                                        <p className='data-field-header'>Nazwisko</p>
+                                    </Grid.Column >
+                                    <Grid.Column width={3}>
+                                        <Input
+                                            type="text"
+                                            name='lastname'
+                                            value={values.lastname}
+                                            onChange={(e, data) => this.handleChange(e, data, setFieldValue)} />
+                                        {errors.lastname && touched.lastname ? <div><CustomLabel text={errors.lastname}></CustomLabel></div> : null}
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column width={2}>
+                                        <p className='data-field-header'>Numer ewidencyjny</p>
+                                    </Grid.Column >
+                                    <Grid.Column width={3}>
+                                        <Input
+                                            type="number"
+                                            name='regnum'
+                                            value={values.regnum}
+                                            onChange={(e, data) => this.handleChange(e, data, setFieldValue)}
+                                        />
+                                        {errors.regnum && touched.regnum ? <div><CustomLabel text={errors.regnum}></CustomLabel></div> : null}
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column width={2}>
+                                        <p className='data-field-header'>Email</p>
+                                    </Grid.Column >
+                                    <Grid.Column width={3}>
+                                        <Input
+                                            type="email"
+                                            name='email'
+                                            value={values.email}
+                                            onChange={(e, data) => this.handleChange(e, data, setFieldValue)} />
+                                        {errors.email && touched.email ? <div><CustomLabel text={errors.email}></CustomLabel></div> : null}
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column width={2}>
+                                        <p className='data-field-header'>Stanowisko</p>
+                                    </Grid.Column >
+                                    <Grid.Column width={3}>
+                                        <Input
+                                            type="text"
+                                            name='position'
+                                            value={values.position}
+                                            onChange={(e, data) => this.handleChange(e, data, setFieldValue)} />
+                                        {errors.position && touched.position ? <div><CustomLabel text={errors.position}></CustomLabel></div> : null}
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column width={2}>
+                                        <p className='data-field-header'>Przełożony</p>
+                                    </Grid.Column >
+                                    <Grid.Column width={3}>
+                                        <Dropdown fluid selection
+                                            name='manager'
+                                            className=''
+                                            placeholder="Wybierz.."
+                                            value={values.manager}
+                                            onChange={(e, data) => this.handleChange(e, data, setFieldValue)}
+                                            options={this.props.users} />
+                                        {errors.manager && touched.manager ? <div><CustomLabel text={errors.manager}></CustomLabel></div> : null}
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column width={2}>
+                                        <p className='data-field-header'>Rola użytkownika</p>
+                                    </Grid.Column >
+                                    <Grid.Column width={3}>
+                                        <Dropdown fluid selection
+                                            name='roles'
+                                            className=''
+                                            placeholder="Wybierz.."
+                                            value={values.roles}
+                                            onChange={(e, data) => this.handleChange(e, data, setFieldValue)}
+                                            options={this.props.roles} />
+                                        {errors.roles && touched.roles ? <div><CustomLabel text={errors.roles}></CustomLabel></div> : null}
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column width={2}>
+                                        <p className='data-field-header'>Aktywny</p>
+                                    </Grid.Column >
+                                    <Grid.Column width={3}>
+                                        <Checkbox
+                                            name='active'
+                                            className='checkbox-active'
+                                            checked={values.active}
+                                            onClick={(e, data) => this.handleChange(e, data, setFieldValue)}
+                                        />
+                                    </Grid.Column>
+                                </Grid.Row>
 
 
-                                </Grid>
-                                <div className='button-row'>
-                                    <Button
-                                        type='submit'
-                                        className='saveButton'
-                                        color='teal'>
-                                        Zapisz
+
+
+                            </Grid>
+                            <div className='button-row'>
+                                <Button
+                                    type='submit'
+                                    className='saveButton'
+                                    color='teal'>
+                                    Zapisz
                                 </Button>
-                                </div>
-                            </Form>
-                        )}
+                            </div>
+                        </Form>
+                    )}
                 </Formik>
             </Segment>
 
